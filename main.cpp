3f51104e6c8975d46bc214c969ab927fbf87873c
Johann Hahn <wanische@gmail.com>
2025-12-29 21:03:06 +0100

    refactor ongoing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-#include <chrono>
 #include <print>
 #include <stdint.h>
-#include <thread>
 #include "d3.hpp"
 #include <gmath/gmath.hpp>
 
@@ -14,18 +12,7 @@ constexpr uint64_t window_height = 1024;
 float angle = 0.f;
 float z = 0.f;
 
-
-
-
-
-
 d3::RectangleI rec = {window_width - 200, 150, 100, 100};
-constexpr d3::Color BLACK = {0, 0, 0, 255};
-constexpr d3::Color RED = {255, 0, 0, 255};
-constexpr d3::Color GREEN = {0, 255, 0, 255};
-constexpr d3::Color BLUE = {0, 0, 255, 255};
-
-
 
 bool index_test(float u, float v, int width, int height) {
     size_t index = u * (width - 1) + v * (height - 1) * width;
@@ -62,56 +49,7 @@ bool index_test(float u, float v, int width, int height) {
     return succ;
 }
 
-
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    using namespace std::chrono_literals;
-
-    assert(index_test(0, 0, 2048, 2048) && "index test failed");
-
-    
-    d3::Window window(window_width, window_height, window_title, hInstance);
-    window.target_fps = 100;
-    d3::Object object;
-    window.objects.push_back(object);
-    Vec3 a = {};
-    float l = a.length();
-    std::println("l = {}", l);
-
-    //window.vertices.push_back({{50, window_height / 2.f, 0.f}, 0.f, 0.f, 0});
-    //window.vertices.push_back({{window_width / 2.f, window.height - 50.f, 0,}, 0.f, 1.f, 0});
-    //window.vertices.push_back({{window_width / 2.f, 50, 0}, 1.f, 0.f, 0});
-    //window.vertices.push_back({{window_width - 50, window_height / 2.f, 0}, 1.f, 1.f, 0});
-
-    window.vertices.push_back({{50, 50, 0.f}, 0.f, 0.f, 0});
-    window.vertices.push_back({{200.f , window.height - 200.f, 0,}, 0.f, 1.f, 0});
-    window.vertices.push_back({{window_width - 200, 50, 0}, 1.f, 0.f, 0});
-    window.vertices.push_back({{window_width - 200, window_height - 200, 0}, 1.f, 1.f, 0});
-
-    window.indeces.push_back(0);
-    window.indeces.push_back(1);
-    window.indeces.push_back(2);
-
-    window.indeces.push_back(1);
-    window.indeces.push_back(2);
-    window.indeces.push_back(3);
-
-    window.show(nCmdShow);
-	MSG message = {0};
-
-
-    timeBeginPeriod(1);
-    std::chrono::milliseconds frametime((uint64_t)(1000.f / window.target_fps));
-    std::chrono::milliseconds elapsed_total;
-
-
-    while (message.message != WM_QUIT) {
-
-        auto begin = std::chrono::steady_clock::now();
-
-        while (PeekMessage(&message, nullptr, 0,0, PM_REMOVE)) {
-            TranslateMessage(&message);
-            DispatchMessage(&message);
-        }
+void controls(d3::Window& window) {
 
 	int speed = 500;
 	int step = 1;//speed * window.target_fps / 1000.f;
@@ -150,32 +88,59 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLin
 	    }
 	    angle -= 0.01f;
 	}
-	if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) {
-	    PostMessage(window.hwnd, WM_CLOSE, 0, 0);
-	}
 	window.vertices[3].pos.x = rec.x;
 	window.vertices[3].pos.y = rec.y;
-	
-	window.clear_pixels(BLACK);
+}
+
+
+int main() {
+//int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+
+    assert(index_test(0, 0, 2048, 2048) && "index test failed");
+
+    timeBeginPeriod(1);
+
+    
+    d3::Window window(window_width, window_height, window_title);
+    window.target_fps = 100;
+    d3::Object object;
+    window.objects.push_back(object);
+    //window.vertices.push_back({{50, window_height / 2.f, 0.f}, 0.f, 0.f, 0});
+    //window.vertices.push_back({{window_width / 2.f, window.height - 50.f, 0,}, 0.f, 1.f, 0});
+    //window.vertices.push_back({{window_width / 2.f, 50, 0}, 1.f, 0.f, 0});
+    //window.vertices.push_back({{window_width - 50, window_height / 2.f, 0}, 1.f, 1.f, 0});
+
+    window.vertices.push_back({{50, 50, 0.f}, 0.f, 0.f, 0});
+    window.vertices.push_back({{200.f , window.height - 200.f, 0,}, 0.f, 1.f, 0});
+    window.vertices.push_back({{window_width - 200, 50, 0}, 1.f, 0.f, 0});
+    window.vertices.push_back({{window_width - 200, window_height - 200, 0}, 1.f, 1.f, 0});
+
+    window.indeces.push_back(0);
+    window.indeces.push_back(1);
+    window.indeces.push_back(2);
+
+    window.indeces.push_back(1);
+    window.indeces.push_back(2);
+    window.indeces.push_back(3);
+
+
+
+    while(window.is_open) {
+
+	controls(window);
+
+	window.begin_frame();
 
 	window.draw_rec(rec, {0xFF, 0x00, 0x22, 0xFF} );
 
-	window.draw_triangles();
-	//window.vertices[3].pos.x = rec.x;
-	//window.vertices[3].pos.y = rec.y;
+	//window.draw_triangles();
+
 	window.render_screen();
 
-	auto end = std::chrono::steady_clock::now();
-	auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-	//std::println("frame time = {}", elapsed);
-	std::chrono::milliseconds wait_time = frametime - elapsed;
-	//std::println("wait time = {}", wait_time);
-	if (wait_time > 0ms) std::this_thread::sleep_for(wait_time);
+	window.end_frame();
 
-	elapsed_total = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - begin);
     }
 
-    timeEndPeriod(1);
 
-    return (int)message.wParam;
+    return 0;
 }
